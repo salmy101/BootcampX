77a3c57891469ab0bfa5d4cf0e5ae82e1f31b574
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-30 14:55:36 +0000

    Parameteriz the  Queries to prevent SQLinjection attacks

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,41 +1,30 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
+const pool = new Pool({ //configuration data for our database as a new pool constant
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-// pool.connect(() => {
-//   console.log("connected to database")
-// });
 
-pool.query(`
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5}
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-  pool.end();
+res.rows.forEach(user => {
+  console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+})
+pool.end();
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
